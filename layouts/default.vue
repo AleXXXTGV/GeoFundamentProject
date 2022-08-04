9ba7fb7cd3f182fdae76c0cef5b9fc4129bc976c
AleXXXTGV <alekstagaev17@gmail.com>
2022-08-05 01:21:44 +0500

    footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
-        <header class="h-90 bg-rgba(155, 152, 138, 1) w-full">
+        <header class="h-90 bg-header-color w-full">
         <div class="container min-h-full flex justify-between items-center">
             <div class="logo">
-                <!-- <img src="./imgs/logo.png" alt="logo"> -->
+                <img src="~/static/imgs/logo.png" alt="logo">
             </div>
-            <nav class="header__menu w-3/6">
-                <ul class="flex font-bold text-white w-full justify-between">
-                    <li><a href="#hero" class=" scroll-to">Главная</a></li>
-                    <li><a href="#whatWeDo" class=" scroll-to">Что делаем</a></li>
-                    <li><a href="#conference" class=" scroll-to">Конференция</a></li>
-                    <li><a href="#about" class=" scroll-to">О нас</a></li>
+            <nav class="header__menu w-3/6 flex justify-center">
+                <ul class="flex font-bold text-white w-full max-w-md justify-between">
+                    <li><nuxt-link to="/">Главная</nuxt-link></li>
+                    <li><nuxt-link to="/">Конференция</nuxt-link></li>
+                    <li><nuxt-link to="/about">О нас</nuxt-link></li>
                 </ul>
             </nav>
             <div class="languages text-white">
@@ -21,7 +20,30 @@
         </div>
     </header>
         <Nuxt />
-        <footer>Footer</footer>
+        <footer class="py-53px pl-40px pr-143px bg-footer-color w-full">
+            <div class="container flex justify-between">
+                <div class="map w-1/2">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1536.3794930518904!2d66.94129797499326!3d39.63263021146373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f4d1ed1bc071c3f%3A0xb116f048376cb623!2sSamarqand%20Davlat%20Arxitektura-qurilish%20Instituti!5e0!3m2!1sru!2s!4v1659639435155!5m2!1sru!2s" width="100%" height="322px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="w-full"></iframe>
+            </div>
+            <div class="contacts w-1/3 flex flex-col justify-between">
+                <div><span>Наши контакты</span></div>
+                <div class="flex justify-between w-full">
+                    <div class="">
+                        <span class="block">+99897 923 33 73</span>
+                        <span class="block">+99897 927 37 33</span>
+                        <span class="block">+99897 399 89 89</span>
+                    </div>
+                    <div class="flex max-w-110 flex-wrap">
+                        <div><img src="~/static/imgs/facebook.svg" alt=""></div>
+                        <div><img src="~/static/imgs/telegram.svg" alt=""></div>
+                        <div><img src="~/static/imgs/instagram.svg" alt=""></div>
+                        <div><img src="~/static/imgs/youtube.svg" alt=""></div>
+                    </div>
+                </div>
+                <div><span>uzssmge@gmail.com</span><img src="" alt=""></div>
+            </div>
+            </div>
+        </footer>
     </div>
 </template>
 <script>
@@ -29,9 +51,5 @@ export default {
     
 }
 </script>
-<style>
-    footer {
-        background-color: green;
-        height: 90px;
-    }
+<style> 
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,20 @@ module.exports = {
     `app.{js,ts,vue}`
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-color': 'rgba(155, 152, 138, 1)',
+        'footer-color': 'rgba(206, 173, 141, 1)',
+      },
+      padding: {
+        '53px': '53px',
+        '40px': '40px',
+        '143px': '143px',
+      },
+      maxWidth: {
+        '110': '110px',
+      },
+    },
     container: {
       center: true,
     },
