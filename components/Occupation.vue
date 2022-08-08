462e99805ef8cc5755117b284bdd3cf882b238d3
Alex Tagaev <56753486+AleXXXTGV@users.noreply.github.com>
2022-08-08 19:25:00 +0500

    Update Occupation.vue

--- components/Occupation.vue
+++ components/Occupation.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="whatAreWeDoing bg-body-color py-60px h-1200 flex">
       <div class="container flex justify-between content-between flex-wrap">
-        <div class="item bg-item1 bg-cover bg-no-repeat bg-center max-w-580 h-307 rounded w-1/2 bg-black">
+        <div class="item bg-item1 bg-cover bg-no-repeat bg-center w-47.5 h-307 rounded bg-black">
           <div class="container relative h-full">
             <div class="item__title absolute bottom-14.5 w-full border-solid border-item-line border-b pb-2">
               <span class="font-bold text-white">Геодезия и топография</span>
             </div>
           </div>
         </div>
-        <div class="item bg-item2 bg-cover bg-no-repeat bg-center max-w-580 h-307 rounded w-1/2 bg-black">
+        <div class="item bg-item2 bg-cover bg-no-repeat bg-center w-47.5 h-307 rounded bg-black">
           <div class="container relative h-full">
             <div class="item__title absolute bottom-14.5 w-full border-solid border-item-line border-b pb-2">
               <span class="font-bold text-white">Геотехнические исследование</span>
             </div>
           </div>
         </div>
-        <div class="item bg-item3 bg-cover bg-no-repeat bg-center max-w-580 h-307 rounded w-1/2 bg-black">
+        <div class="item bg-item3 bg-cover bg-no-repeat bg-center w-47.5 h-307 rounded bg-black">
           <div class="container relative h-full">
             <div class="item__title absolute bottom-14.5 w-full border-solid border-item-line border-b pb-2">
               <span class="font-bold text-white">Комлексное проектирование</span>
             </div>
           </div>
         </div>
-        <div class="item bg-item4 bg-cover bg-no-repeat bg-center max-w-580 h-307 rounded w-1/2 bg-black">
+        <div class="item bg-item4 bg-cover bg-no-repeat bg-center w-47.5 h-307 rounded bg-black">
           <div class="container relative h-full">
             <div class="item__title absolute bottom-14.5 w-full border-solid border-item-line border-b pb-2">
               <span class="font-bold text-white">Буравые работы</span>
