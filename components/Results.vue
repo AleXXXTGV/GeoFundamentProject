b6588df04bf864486ff74648547e7947a7dcfdab
AleXXXTGV <alekstagaev17@gmail.com>
2022-08-13 22:08:32 +0500

    not finish

--- components/Results.vue
+++ components/Results.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="results bg-body-color flex flex-col text-white pt-60px hidden">
+    <div class="results bg-body-color flex flex-col text-white pt-60px">
         <span class="font-bold text-4xl self-center raleway">Наши результаты </span>
         <div ref="observer"
             class="bg-results bg-cover bg-no-repeat bg-center h-540 mt-60px flex justify-center items-center ">
             <div class="container h-90p flex bg-results-container items-center pl-30px">
                 <div class="w-26.5 flex flex-col text-center">
-                    <span class="block font-bold text-220 w-313px"><span class="count_number">33</span></span>
-                    <span class="block font-bold text-40 raleway">года на рынке</span>
+                    <span class="font-bold flex items-centerlg:text-140 xl:text-220 w-313px"><span class="count_number">33</span></span>
+                    <span class="block font-bold lg:text-3xl xl:text-40 raleway">года на рынке</span>
                 </div>
                 <div class="flex flex-wrap justify-end h-75p content-between">
                     <div class="flex justify-between w-90p">
                         <div class="flex flex-col w-min justify-between max-w-268">
-                            <span class="block font-bold text-40"><span class="count_number">1000</span>+</span>
-                            <span class="block text-24  raleway">Телекомуникационных компании</span>
+                            <span class="flex items-centerlg:text-3xl font-bold xl:text-40"><span class="count_number">1000</span>+</span>
+                            <span class="block lg:text-2xl xl:text-24  raleway">Телекомуникационных компании</span>
                         </div>
                         <div class="flex flex-col w-min justify-between max-w-268">
-                            <span class="block font-bold text-40"><span class="count_number">500</span>+</span>
-                            <span class="block text-24  raleway">Министерства обороны РУ</span>
+                            <span class="flex items-centerlg:text-3xl font-bold xl:text-40"><span class="count_number">500</span>+</span>
+                            <span class="block lg:text-2xl xl:text-24  raleway">Министерства обороны РУ</span>
                         </div>
                         <div class="flex flex-col justify-between max-w-268">
-                            <span class="block font-bold text-40"><span class="count_number">7000</span>+</span>
-                            <span class="block text-24  raleway">Министерство образования РУ</span>
+                            <span class="flex items-centerlg:text-3xl font-bold xl:text-40"><span class="count_number">7000</span>+</span>
+                            <span class="block lg:text-2xl xl:text-24  raleway">Министерство образования РУ</span>
                         </div>
                     </div>
                     <div class="flex justify-between w-90p">
                         <div class="flex flex-col justify-between max-w-268">
-                            <span class="block font-bold text-40"><span class="count_number">20</span>+</span>
-                            <span class="block text-24  raleway">Нефтегазовые, горно-металлкургические
+                            <span class="flex items-centerlg:text-3xl font-bold xl:text-40"><span class="count_number">20</span>+</span>
+                            <span class="block lg:text-2xl xl:text-24  raleway">Нефтегазовые, горно-металлкургические
                                 компании</span>
                         </div>
                         <div class="flex flex-col justify-between max-w-268">
-                            <span class="block font-bold text-40"><span class="count_number">40</span>+</span>
-                            <span class="block text-24  raleway">Количество <br> Спец. техники</span>
+                            <span class="flex items-centerlg:text-3xl font-bold xl:text-40"><span class="count_number">40</span>+</span>
+                            <span class="block lg:text-2xl xl:text-24  raleway">Количество <br> Спец. техники</span>
                         </div>
                         <div class="flex flex-col justify-between max-w-268">
-                            <span class="block font-bold text-40"><span class="count_number">10000</span>+</span>
-                            <span class="block text-24  raleway">Инжинеринговые компании областных мэрий РУ</span>
+                            <span class="flex items-centerlg:text-3xl font-bold xl:text-40"><span class="count_number">10000</span>+</span>
+                            <span class="block lg:text-2xl xl:text-24  raleway">Инжинеринговые компании областных мэрий РУ</span>
                         </div>
                     </div>
                 </div>
